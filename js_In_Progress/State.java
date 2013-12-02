a912dfc6e69e32adc6a3a8edbe59ca6916f22b03
Rob Taylor <robkcseattle@gmail.com>
2013-12-02 14:28:40 -0800

    Added image file names to array and state class

--- js_In_Progress/State.java
+++ js_In_Progress/State.java
@@ -3,16 +3,16 @@ public class State {
 	private String _capitol;
 	private String _city1;
 	private String _city2;
-	//private String _image; This needs to be the path to the file, but not sure yet how to return it
+	private String _image; //file name of the state image
 	
 	
-	public State(String name, String capitol, String, city1, String city2)
+	public State(String name, String capitol, String, city1, String city2, String image)
 	{
 		_name = name;
 		_capitol = capitol;
 		_city1 = city1;
 		_city2 = city2;
-		//Will need to change this method to include the image
+		_image = image;
 	}
 	public String toString()
 	{
@@ -20,6 +20,6 @@ public class State {
 		return _capitol;
 		return _city1;
 		return _city2;
-		//return _image; Will try to implement the name part of this first, then come back to add the image.
+		return _image;
 	}
 }
