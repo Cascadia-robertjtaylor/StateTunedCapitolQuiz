c62a0ee6611d08414d231c03c4ddf7fb90b16970
Shyawn Karim <shyawnkarim@yahoo.com>
2013-12-05 17:37:01 -0800

    State class
    Fixed the state class return values to return a string

--- StateTunedCapitolQuiz/src/com/example/statetunedcapitolquiz/State.java
+++ StateTunedCapitolQuiz/src/com/example/statetunedcapitolquiz/State.java
@@ -1,5 +1,7 @@
 package com.example.statetunedcapitolquiz;
 
+import java.util.ArrayList;
+
 public class State {
 	private String _name;
 	private String _capitol;
@@ -16,14 +18,73 @@ public class State {
 		_city2 = city2;
 		_image = image;
 	}
+	
+
+	public String getName() {
+		   return _name;
+		 }
+	 
+	 public String getCapitol() {
+		   return _capitol;
+		 }
+	 
+	 public String getCity1() {
+		   return _city1;
+		 }
+	 
+	 public String getCity2() {
+		   return _city2;
+		 }
+	 
+	 public String getImage() {
+		   return _image;
+		 }
+	
+	 /*public ArrayList<State> getName() {
+		   return _name;
+		 }
+	 
+	 public ArrayList<State> getCapitol() {
+		   return _capitol;
+		 }
+	 
+	 public ArrayList<State> getCity1() {
+		   return _city1;
+		 }
+	 
+	 public ArrayList<State> getCity2() {
+		   return _city2;
+		 }
+	 
+	 public ArrayList<State> getImage() {
+		   return _image;
+		 }*/
+	
 	/*public String toString()
 	{
-		return _name,
-			_capitol,
-			_city1,
-			_city2,
-			_image;
+		return 	_name;
+	}
+	
+	public String toString()
+	{
+		return _capitol;
+	}
+	
+	public String toString()
+	{
+		return _city1;
+	}
+	
+	public String toString()
+	{
+		return _city2;
+	}
+	
+	public String toString()
+	{
+		return _image;
 	}*/
+	
 }
 
 
